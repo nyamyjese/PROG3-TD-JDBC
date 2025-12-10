ab3bd4e3720aa1d3c2537cc2cda804a68e9984b6
Ny Amy Jese <hei.jese.2@gmail.com>
2025-12-10 09:03:15 +0300

    feat: add Main class for the tests

--- src/main/java/org/TD/Main.java
+++ src/main/java/org/TD/Main.java
@@ -1,8 +1,54 @@
 package org.TD;
 
+import java.sql.SQLException;
+import java.time.Instant;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
+        DataRetriever dataRetriever = new DataRetriever();
+
+        try {
+            List<Category> categories = dataRetriever.getAllCategories();
+            for (Category c : categories) {
+                System.out.println(c.getId() + " | " + c.getName());
+            }
+
+            List<Product> productsPage1 = dataRetriever.getProductList(1, 5);
+            for (Product p : productsPage1) {
+                System.out.println(
+                        p.getId() + " | " +
+                        p.getName() + " | " +
+                        p.getCreationDatetime() + " | "
+                );
+            }
+
+            List<Product> filtered = dataRetriever.getProductsByCriteria(
+                    "a",
+                    null,
+                    null,
+                    null
+            );
+            for (Product p : filtered) {
+                System.out.println(p.getId() + " - " + p.getName());
+            }
+
+            List<Product> filteredPaginated = dataRetriever.getProductsByCriteria(
+                    "a",
+                    null,
+                    null,
+                    null,
+                    1,   // page
+                    3    // size
+            );
+
+            for (Product p : filteredPaginated) {
+                System.out.println(p.getId() + " | " + p.getName());
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Error SQL : " + e.getMessage());
+        }
     }
 }
